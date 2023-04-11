0bdde69b48553926abd63cc422b3ba55b07007af
Thiago Camerato <104781163+thiagocamerato757@users.noreply.github.com>
2023-04-11 14:13:53 -0300

    Add files via upload

---
+++ tarefa01/meu_header.h
@@ -0,0 +1,9 @@
+
+//2212580 - Thiago Pereira Camerato 33B
+
+//functions prototypes
+int ** criaMatriz(int a, int b);
+void imprimeMatriz2D(int ** c,int a ,int b);
+void liberaMemoria (int ** c, int a);
+int alteraMatriz2D(int **c, int q, int a , int b);
+int verificaDiagonalidade(int** c, int a, int b, int bool );
