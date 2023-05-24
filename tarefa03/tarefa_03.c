16fec2e9fc3a8d7d0b07e1c92c0178a8104f8756
thiagocamerato757 <cameratothiago@gmail.com>
2023-05-24 15:32:01 -0300

    updated in 24/05/2023

--- tarefa03/tarefa_03.c
+++ tarefa03/tarefa_03.c
@@ -29,6 +29,8 @@ Client** lerMontarVetor(FILE* file, int linhas);
 void imprimeVetor(Client** clients, int qtd);
 float mean(Client ** p, int qtd);
 float standardDeviation(Client** data, int qtd , float media);
+int comparaAltura(Client* clienteA, float clienteB);
+int buscaBinaria(Client** vetor, int qtd, float altura);
 void freeMem(Client** cl, int lines);
 
 
@@ -40,16 +42,36 @@ int main(void) {
     Client** vector;
     float mediaPeso;
     float desvioPadrao;
+    int indicePessoaMaiorPeso;
+    float alturaEspecificada;
 
     //execution
-    lines = countLines("clientes.txt");
     arqText = abrearq("clientes.txt", "r");
+    printf(" Clientes :\n");
+    lines = countLines("clientes.txt");
     vector = lerMontarVetor(arqText, lines);
     imprimeVetor(vector, lines);
     mediaPeso = mean(vector,lines);
-    printf("Média do peso : %.2f kg",mediaPeso);
-    desvioPadrao = standardDeviation(vector,lines,mediaPeso);
-    printf("     Desvio padrão do peso : %.2f\n",desvioPadrao);
+    if(lines == 0){
+        printf("Média e Desvio padrão não calculável(divisão por zero !)\n");
+    }
+    else{
+        printf("Média do peso : %.2f kg",mediaPeso);
+        desvioPadrao = standardDeviation(vector,lines,mediaPeso);
+        printf("     Desvio padrão do peso : %.2f\n",desvioPadrao);
+        printf("Digite a altura para procurarmos a pessoa com o maior peso dessa altura!\n");
+        scanf("%f",&alturaEspecificada);
+        indicePessoaMaiorPeso = buscaBinaria(vector,lines,alturaEspecificada);
+        if (indicePessoaMaiorPeso != -1) {
+        printf("Registro da pessoa de maior peso com altura %.2f: %s, %.2f m %.1f kg\n", alturaEspecificada,
+               vector[indicePessoaMaiorPeso]->nome, vector[indicePessoaMaiorPeso]->info.altura, 
+               (float)vector[indicePessoaMaiorPeso]->info.peso);
+    } 
+    else {
+    printf("Nenhuma pessoa encontrada com a altura %.2f\n m ", alturaEspecificada);
+    }   
+    }
+   
 
     freeMem(vector, lines);
     fclose(arqText);
@@ -76,8 +98,7 @@ int countLines(char * fName) {
             contaLinha++;
         }
     }
-    contaLinha++;
-    fclose(arq);
+
     return contaLinha;
 }
 //function that creates an array of pointers and includes all related data in it 
@@ -117,43 +138,116 @@ Client** lerMontarVetor(FILE* file, int linhas) {
 
 //function to print the array of pointers
 void imprimeVetor(Client** clients, int qtd) {
-    printf(" Clientes :\n");
-    printf(" --------------------------------------\n");
-    for (int i = 0; i < qtd; i++) {
-        printf("| %-15s-> %6.2f m %6.1f kg |\n", clients[i]->nome, clients[i]->info.altura, (float)clients[i]->info.peso);
+    if (qtd == 0){
+            printf("O arquivo está vazio!\n");
+            return;
     }
-    printf(" --------------------------------------\n");
+    else{
+        printf(" --------------------------------------\n");
+        for (int i = 0; i < qtd; i++) {
+     
+        printf("| %-15s-> %6.2f m %6.1f kg |\n", clients[i]->nome, clients[i]->info.altura, (float)clients[i]->info.peso);
+        }
+        printf(" --------------------------------------\n");
+    }
+    
 }
 
 //function to calculate the mean of the weights 
 float mean(Client ** p, int qtd){
-    float soma = 0;
-    for(int i = 0; i < qtd; i++ ){
-        soma += (float) p[i]->info.peso;
+    if (qtd == 0){
+        return 0.0;
     }
-    return (soma/(float)qtd);
+    else{
+        float soma = 0;
+        for(int i = 0; i < qtd; i++ ){
+            soma += (float) p[i]->info.peso;
+        }
+        return (soma/(float)qtd);
+    }
+   
 }
 
 //function to calculate the standart Deviation of the weights
 float standardDeviation(Client** data, int qtd , float media) {
     float varia = 0.0;
 
-    for (int i = 0; i < qtd; i++) {
-        varia += (data[i]->info.peso - media) * (data[i]->info.peso - media);
+    if(qtd == 0){
+        return 0;
+    }
+    else{
+
+        for (int i = 0; i < qtd; i++) {
+            varia += (data[i]->info.peso - media) * (data[i]->info.peso - media);
+        }
+
+        varia /= qtd;
+        float stdDeviation = 0;
+
+        for (int i = 0; i < qtd; i++) {
+            stdDeviation += (data[i]->info.peso - media) * (data[i]->info.peso - media);
+        }
+
+        stdDeviation = sqrt(stdDeviation / qtd);
+        return stdDeviation;
     }
 
-    varia /= qtd;
-    float stdDeviation = 0;
-
-    for (int i = 0; i < qtd; i++) {
-        stdDeviation += (data[i]->info.peso - media) * (data[i]->info.peso - media);
-    }
-
-    stdDeviation = sqrt(stdDeviation / qtd);
-    return stdDeviation;
 }
 
-//function to free the dynamic alocated memory
+// Compare the heights of two clients
+// Returns:
+//   -1 if clienteA's height is smaller than clienteB's height
+//    1 if clienteA's height is greater than clienteB's height
+//    0 if clienteA's height is equal to clienteB's height
+int comparaAltura(Client* clienteA, float clienteB) {
+    // Comparison of heights
+    if (clienteA->info.altura < clienteB){
+        return -1;
+    } else if (clienteA->info.altura > clienteB ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Binary search to find the index of the person with the highest weight at a given height
+// Returns the index of the person with the highest weight at the specified height
+// If no person is found with the specified height, returns -1
+int buscaBinaria(Client** vetor, int qtd, float altura) {
+    int inicio = 0;
+    int fim = qtd - 1;
+    
+    while (inicio <= fim) {
+        int meio = (inicio + fim) / 2;
+
+        // Check if the height at the middle index matches the specified height
+        if (comparaAltura(vetor[meio], altura) == 0) {
+            // Sequential search to find the person with the highest weight for the specified height
+            int indice = meio;
+            while (indice < fim && comparaAltura(vetor[indice + 1], altura) == 0) {
+                if (vetor[indice + 1]->info.peso > vetor[indice]->info.peso) {
+                    indice++;
+                } else {
+                    break;
+                }
+            }
+            return indice;
+        }
+
+        // Check if the height at the middle index is greater than the specified height
+        if (comparaAltura(vetor[meio], altura) > 0) {
+            fim = meio - 1;
+        }
+        // Check if the height at the middle index is less than the specified height
+        else {
+            inicio = meio + 1;
+        }
+    }
+
+    return -1; // Height not found
+}
+
+//function to dealocate the dynamic alocated memory
 void freeMem(Client** cl, int lines) {
     for (int i = 0; i < lines; i++) {
         free(cl[i]);
