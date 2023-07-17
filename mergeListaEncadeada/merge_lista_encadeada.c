6c374b3cd30ce9279699fcbe7f7be0001e5eb3cd
Thiago Camerato <cameratothiago@gmail.com>
2023-07-17 15:25:07 -0300

    updated in 17/07/2023

--- mergeListaEncadeada/merge_lista_encadeada.c
+++ mergeListaEncadeada/merge_lista_encadeada.c
@@ -1,14 +1,57 @@
+//includes
 #include <stdio.h>
 #include <stdlib.h>
 
-// Definição da estrutura do nó da lista
+//structed data
 typedef struct Node {
     int codigo;
     char* nome;
     struct Node* prox;
 } Node;
 
-// Função para criar um novo nó
+//function prototypes
+Node* criarNo(int codigo, char* nome);
+Node* MergeListas(Node* lista1, Node* lista2);
+void imprimirLista(Node* lista);
+void imprimirLista(Node* lista);
+void liberarLista(Node* lista);
+
+//main function
+int main(void) {
+    //variables
+    Node* lista1;
+    Node* lista2;
+    Node* listaResultante;
+    
+    //execution
+
+    lista1 = criarNo(111, "leo");
+    lista1->prox = criarNo(333, "rui");
+    lista1->prox->prox = criarNo(446, "cris");
+    lista1->prox->prox->prox = criarNo(558, "lara");
+    lista1->prox->prox->prox->prox = criarNo(666, "cadu");
+    lista1->prox->prox->prox->prox->prox = criarNo(777, "rosa");
+    lista1->prox->prox->prox->prox->prox->prox = criarNo(999, "bia");
+
+    lista2 = criarNo(111, "leo");
+    lista2->prox = criarNo(333, "rui");
+    lista2->prox->prox = criarNo(440, "edu");
+    lista2->prox->prox->prox = criarNo(554, "vera");
+    lista2->prox->prox->prox->prox = criarNo(777, "rosa");
+    lista2->prox->prox->prox->prox->prox = criarNo(888, "lia");
+
+    //merging two linked lists
+    listaResultante = MergeListas(lista1, lista2);
+
+    imprimirLista(listaResultante);
+
+    //deallocating dynamic allocated memory
+    liberarLista(listaResultante);
+
+    return 0;
+}
+
+//function to create a new node
 Node* criarNo(int codigo, char* nome) {
     Node* novoNo = (Node*)malloc(sizeof(Node));
     if (novoNo == NULL) {
@@ -21,11 +64,12 @@ Node* criarNo(int codigo, char* nome) {
     return novoNo;
 }
 
-// Função para inserir um novo nó na lista resultante
+//function to insert a node into the merged list 
 void inserirNo(Node** listaResultante, Node* novoNo) {
     if (*listaResultante == NULL) {
         *listaResultante = novoNo;
-    } else {
+    } 
+    else {
         Node* atual = *listaResultante;
         Node* anterior = NULL;
         while (atual != NULL && atual->codigo < novoNo->codigo) {
@@ -34,11 +78,13 @@ void inserirNo(Node** listaResultante, Node* novoNo) {
         }
         if (atual != NULL && atual->codigo == novoNo->codigo) {
             free(novoNo); // Descarta nós com códigos repetidos
-        } else {
+        } 
+        else {
             if (anterior == NULL) {
                 novoNo->prox = *listaResultante;
                 *listaResultante = novoNo;
-            } else {
+            } 
+            else {
                 anterior->prox = novoNo;
                 novoNo->prox = atual;
             }
@@ -46,7 +92,7 @@ void inserirNo(Node** listaResultante, Node* novoNo) {
     }
 }
 
-// Função para mesclar duas listas ordenadas
+//function to merge two ordenated linked lists 
 Node* MergeListas(Node* lista1, Node* lista2) {
     Node* listaResultante = NULL;
 
@@ -54,10 +100,12 @@ Node* MergeListas(Node* lista1, Node* lista2) {
         if (lista1->codigo < lista2->codigo) {
             inserirNo(&listaResultante, criarNo(lista1->codigo, lista1->nome));
             lista1 = lista1->prox;
-        } else if (lista1->codigo > lista2->codigo) {
+        } 
+        else if (lista1->codigo > lista2->codigo) {
             inserirNo(&listaResultante, criarNo(lista2->codigo, lista2->nome));
             lista2 = lista2->prox;
-        } else {
+        } 
+        else {
             // Códigos iguais, escolher um dos nós
             inserirNo(&listaResultante, criarNo(lista1->codigo, lista1->nome));
             lista1 = lista1->prox;
@@ -65,13 +113,11 @@ Node* MergeListas(Node* lista1, Node* lista2) {
         }
     }
 
-    // Adicionar os nós restantes da lista1, se houver
     while (lista1 != NULL) {
         inserirNo(&listaResultante, criarNo(lista1->codigo, lista1->nome));
         lista1 = lista1->prox;
     }
 
-    // Adicionar os nós restantes da lista2, se houver
     while (lista2 != NULL) {
         inserirNo(&listaResultante, criarNo(lista2->codigo, lista2->nome));
         lista2 = lista2->prox;
@@ -80,7 +126,7 @@ Node* MergeListas(Node* lista1, Node* lista2) {
     return listaResultante;
 }
 
-// Função para imprimir a lista
+//function to print a linked list 
 void imprimirLista(Node* lista) {
     Node* atual = lista;
     while (atual != NULL) {
@@ -90,7 +136,7 @@ void imprimirLista(Node* lista) {
     printf("\n");
 }
 
-// Função para liberar a memória ocupada pela lista
+//function to free all the dynamic allocated memory
 void liberarLista(Node* lista) {
     Node* atual = lista;
     while (atual != NULL) {
@@ -98,36 +144,4 @@ void liberarLista(Node* lista) {
         free(atual);
         atual = proximo;
     }
-}
-
-int main() {
-    // Exemplo de uso da função MergeListas
-
-    // Criando a lista 1
-    Node* lista1 = criarNo(111, "leo");
-    lista1->prox = criarNo(333, "rui");
-    lista1->prox->prox = criarNo(446, "cris");
-    lista1->prox->prox->prox = criarNo(558, "lara");
-    lista1->prox->prox->prox->prox = criarNo(666, "cadu");
-    lista1->prox->prox->prox->prox->prox = criarNo(777, "rosa");
-    lista1->prox->prox->prox->prox->prox->prox = criarNo(999, "bia");
-
-    // Criando a lista 2
-    Node* lista2 = criarNo(111, "leo");
-    lista2->prox = criarNo(333, "rui");
-    lista2->prox->prox = criarNo(440, "edu");
-    lista2->prox->prox->prox = criarNo(554, "vera");
-    lista2->prox->prox->prox->prox = criarNo(777, "rosa");
-    lista2->prox->prox->prox->prox->prox = criarNo(888, "lia");
-
-    // Mesclando as duas listas
-    Node* listaResultante = MergeListas(lista1, lista2);
-
-    // Imprimindo a lista resultante
-    imprimirLista(listaResultante);
-
-    // Liberando a memória ocupada pela lista
-    liberarLista(listaResultante);
-
-    return 0;
 }
