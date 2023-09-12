c239b7c87ab4fd1a9bed2d3396b6050b603070ad
Thiago Camerato <cameratothiago@gmail.com>
2023-09-11 23:04:50 -0300

    updated in 11/09/2023

--- INF1010/ArvoreBinariaBusca/AUX_arvore_binaria_busca.h
+++ INF1010/ArvoreBinariaBusca/AUX_arvore_binaria_busca.h
@@ -1,8 +1,13 @@
+//structed data
+
+
+//struct to represent a value 
 struct Info
 {
     int numb;
 };typedef struct Info info;
 
+//struct to represent a Binary search tree 
 struct BinarySearchTree
 {
     info inf;
@@ -11,3 +16,9 @@ struct BinarySearchTree
 
 };typedef struct BinarySearchTree BinarySearchTree;
 
+//function prototypes
+BinarySearchTree *createTree(void);
+void addTree(BinarySearchTree *tree, int n);
+int isBalanced(BinarySearchTree *tree);
+BinarySearchTree *deleteBinaryTree(BinarySearchTree *tree, int inf);
+void inorder(BinarySearchTree *tree);
