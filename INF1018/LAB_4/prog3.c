a63821abac45eafca86dd73a0d2fc1301e95e1fb
thiagocamerato757 <cameratothiago@gmail.com>
2023-09-12 14:53:00 -0300

    updated in 12/09/2023

--- INF1018/LAB_4/prog3.c
+++ INF1018/LAB_4/prog3.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
 typedef unsigned packed_t;
-int xbyte(packed_t word, int bytenum) {
-  unsigned char *p = (unsigned char *)&word;
-  return (int)(char)p[bytenum];
+int string2num (char *s, int base) {
+  int a = 0;
+  for (; *s; s++) {
+    if(isdigit(*s))
+      a = a*base + (*s - '0');
+    else if((*s >= 'A') && (*s < (base-10+'A')))
+      a = a*base + ((*s - 'A') + 10);
+    else if((*s >= 'a') && (*s < (base-10+'a')))
+      a = a*base + ((*s - 'a') + 10);
+    else {
+      printf("pane: numero invalido! \n");
+      exit(1);
+    }
+  }
+  return a;
 }
+int xbyte(packed_t word, int bytenum) {
+  if (bytenum < 0 || bytenum >= sizeof(packed_t)) {
+    printf("Invalid byte number!\n");
+    exit(1);
+  }
+  unsigned char byte = (word >> (bytenum * 8)) & 0xFF;
+  // Convert the unsigned byte to a signed integer (assuming 2's complement)
+  int result = (int)(signed char)byte;
+  return result;
+}
+
+
+
 int main(int argc, char **argv) {
   int x;
   if (argc != 3) {
