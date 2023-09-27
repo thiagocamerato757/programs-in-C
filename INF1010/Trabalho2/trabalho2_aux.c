639275dc639f01e1d6a32f1e7ee7fdf0772186a8
Thiago Camerato <cameratothiago@gmail.com>
2023-09-26 22:03:00 -0300

    updated in  26/09/2023

---
+++ INF1010/Trabalho2/trabalho2_aux.c
@@ -0,0 +1,9 @@
+#include "trabalho2_aux.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+abb *iniciatetree(void){
+    return NULL;
+}
+
+
---
+++ INF1010/Trabalho2/trabalho2_aux.h
@@ -0,0 +1,13 @@
+//structed data
+
+//binary search tree like struct
+struct ABB
+{
+    int info;
+    struct ABB *father;
+    struct ABB *left;
+    struct ABB *right;
+}; typedef struct ABB abb;
+
+//function prototypes
+
