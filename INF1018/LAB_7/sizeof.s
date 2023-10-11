d624c104b9e6d385323b66587d49a716e8c21b1b
thiagocamerato757 <cameratothiago@gmail.com>
2023-10-11 16:27:16 -0300

    updated in 11/10/2023

--- INF1018/LAB_7/sizeof.s
+++ INF1018/LAB_7/sizeof.s
@@ -9,27 +9,31 @@ main:
    movq %rsp,%rbp
    subq $16,%rsp
    movq %r12,-8(%rbp)
-   movl $0,%r10d      # contador
-   movl $0,%r11d      # armazena soma
-   movq $nums,%r12    # guarda o vetor
-
-   L1:
-     cmpl $4,%r10d    # Compare r10 to 4
-     je L2            # Jump to L2 if equal
-     movq %r10,%rcx
-     imull $4,%ecx
-     addq (%rcx),%r12
-     addl (%r12),%r11d
-     addl $1, %r10d   # Incrementa  contador
-     jmp L1
-
+   
+   movq $nums,%r12 #array
+   movl $0,%r10d #counter
+   movl $0,%r11d #sum
+   
+  L1:
+      cmpl $4,%r10d
+      jge L2
+      movl %r10d, %ecx
+      movq %r12, %rdx #move to an aux in orden to mantain original address
+      shl $2 ,%ecx
+      addq %rcx,%rdx
+      addl (%rdx),%r11d
+      incl %r10d
+      jmp L1
    L2:
-     movq $Sf,%rdi     
-     movl %r11d,%esi   
-     xorl %eax, %eax
-     call printf
-     movq -8(%rbp),%r12
-     movq %rbp,%rsp
-     popq %rbp
-     ret
+      movl $0, %eax
+      movq $Sf , %rdi
+      movl %r11d, %esi
+      call printf
+    
+     
+      xorl %eax,%eax
+      movq -8(%rbp),%r12
+      movq %rbp,%rsp
+      popq %rbp
+      ret
      
