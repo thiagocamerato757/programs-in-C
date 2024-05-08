69fb29177281cad9e1dd2ee9b3fb74b2dcc91dad
thiagocamerato757 <cameratothiago@gmail.com>
2024-05-07 21:10:00 -0300

    updated inn 07/05/2024

--- INF1316/Trabalho1/escalonador.c
+++ INF1316/Trabalho1/escalonador.c
@@ -3,19 +3,23 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <signal.h>
 #include "interpretador.h"
+
 #define REAL_TIME_MAX_SECONDS 60
 #define ROUND_ROBIN_TIME_INTERVAL 0.5
 
 typedef struct ProgramNode {
     program prog;
+    int globalPriority; // prioridade global - Real time tem prioridade 1, prioridades têm prioridade 2 e round robin tem prioridade 3 
     struct ProgramNode *next;
 } ProgramNode;
 
-void set_timer(int duration) {
-    // Configurar o timer de acordo com a duração do programa
-    alarm(duration);
-}
+// Variáveis globais para uso nos handlers de sinal
+ProgramNode *current_program_node = NULL;
+ProgramNode *real_time_queue = NULL;
+ProgramNode *priority_queues[7] = {NULL};
+ProgramNode *round_robin_queue = NULL;
 
 ProgramNode *create_program_node(program program) {
     ProgramNode *new_node = malloc(sizeof(ProgramNode));
@@ -28,8 +32,9 @@ ProgramNode *create_program_node(program program) {
     return new_node;
 }
 
-void insert_program(ProgramNode **head, program program) {
+void insert_program(ProgramNode **head, program program, int globalPriority) {
     ProgramNode *new_node = create_program_node(program);
+    new_node->globalPriority = globalPriority;
     if (*head == NULL) {
         *head = new_node;
     } else {
@@ -41,60 +46,36 @@ void insert_program(ProgramNode **head, program program) {
     }
 }
 
-void free_program_list(ProgramNode *head) {
-    ProgramNode *current = head;
-    while (current != NULL) {
-        ProgramNode *temp = current;
-        current = current->next;
-        free(temp);
-    }
+void set_timer(int duration) {
+    // Configurar o timer de acordo com a duração do programa
+    alarm(duration);
 }
 
-void print_program_list(ProgramNode *head) {
-    ProgramNode *current = head;
-    while (current != NULL) {
-        printf("Nome: %s, Tipo: %c\n", current->prog.name, current->prog.type);
-        current = current->next;
-    }
-}
-
-int check_real_time_conflict(ProgramNode *queue, program new_program) {
-    while (queue != NULL) {
-        if (new_program.initial_time >= queue->prog.initial_time &&
-            new_program.initial_time < queue->prog.initial_time + queue->prog.duration) {
-            printf("Conflito de escalonamento em tempo real: Novo programa %s com I=%d e D=%d entra em conflito com %s\n",
-                   new_program.name, new_program.initial_time, new_program.duration, queue->prog.name);
-            return 1;
-        } else if (new_program.initial_time + new_program.duration > queue->prog.initial_time &&
-                   new_program.initial_time + new_program.duration <= queue->prog.initial_time + queue->prog.duration) {
-            printf("Conflito de escalonamento em tempo real: Novo programa %s com I=%d e D=%d entra em conflito com %s\n",
-                   new_program.name, new_program.initial_time, new_program.duration, queue->prog.name);
-            return 1;
-        }
-        queue = queue->next;
-    }
-    return 0;
-}
-
-void recebe_info(ProgramNode **real_time_queue, ProgramNode *priority_queues[], ProgramNode **round_robin_queue, int pipe_fd) {
-    printf("Recebendo informações...\n\n");
-    program program;
-    while (read(pipe_fd, &program, sizeof(program)) > 0) {
-        printf("Programa: %s\n", program.name);
-        printf("Tipo: %c\n", program.type);
-        if (program.type == 'P') {
-            printf("Prioridade: %d\n", program.priority);
-            insert_program(&priority_queues[(program.priority -1)], program);
-        } else if (program.type == 'T') {
-            printf("Instante Inicial: %d\n", program.initial_time);
-            printf("Duração: %d\n", program.duration);
-            if (!check_real_time_conflict(*real_time_queue, program)) {
-                insert_program(real_time_queue, program);
-            }
+// Definição dos handlers dos sinais
+void signal_handler(int signum) {
+    // Verificar se há um programa atualmente em execução
+    if (current_program_node != NULL) {
+        // Obter o próximo programa a ser executado
+        ProgramNode *next_program = NULL;
+        if (real_time_queue != NULL) {
+            next_program = real_time_queue;
         } else {
-            insert_program(round_robin_queue, program);
+            for (int i = 0; i < 7; i++) {
+                if (priority_queues[i] != NULL) {
+                    next_program = priority_queues[i];
+                    break;
+                }
+            }
+            if (next_program == NULL && round_robin_queue != NULL) {
+                next_program = round_robin_queue;
+            }
         }
-        printf("\n");
+
+        // Comparar as prioridades globais do programa atual e do próximo programa
+        if (next_program != NULL && next_program->globalPriority < current_program_node->globalPriority) {
+            // Se o próximo programa tiver prioridade global mais alta, enviar um sinal SIGUSR1 para preempção
+            kill(getpid(), SIGUSR1);
+        }
     }
 }
 
@@ -106,8 +87,6 @@ void exec_program(ProgramNode **program_node) {
         perror("Erro ao criar processo filho");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        // Configurar o handler para o sinal de alarme
-        signal(SIGALRM, alarm_handler);
         if (prog.type == 'P') {
             // Implementação para programas de prioridade
             for (int i = 0; i < 6; i++) {
@@ -116,183 +95,82 @@ void exec_program(ProgramNode **program_node) {
             }
             exit(EXIT_SUCCESS);
         } else if (prog.type == 'T') {
-            // Verificar se já existe um programa em execução
-            if (getpid() == 0) {
-                // Configurar o temporizador para o tempo de execução
-                set_timer(prog.duration);
-                // Simular a execução do programa em tempo real
-                while (1) {
-                    // Inserir aqui o código do programa em tempo real
-                }
-            } else {
-                // Pegar o próximo programa da fila de tempo real que ainda não está em execução
-                ProgramNode *next_program = get_next_real_time_program();
-                if (next_program != NULL) {
-                    exec_program(&next_program);
-                } else {
-                    // Se não houver mais programas em tempo real, verificar se há programas round-robin na fila
-                    ProgramNode *round_robin_program = get_next_round_robin_program();
-                    if (round_robin_program != NULL) {
-                        // Executar o programa round-robin por um período determinado pela sua duração
-                        printf("Executando programa Round-Robin: %s\n", round_robin_program->prog.name);
-                        sleep(round_robin_program->prog.duration);
-                    }
+            // Implementação para programas de tempo real
+            while (1) {
+                time_t current_time = time(NULL);
+                struct tm *local_time = localtime(&current_time);
+                if (local_time->tm_sec == prog.initial_time) {
+                    printf("Tempo Real: Executando programa %s\n", prog.name);
+                    sleep(prog.duration);
                 }
             }
         } else if (prog.type == 'R') {
-            // Verificar se já existe um programa em execução
-            if (getpid() == 0) {
-                // Configurar o temporizador para o intervalo de tempo
-                set_timer(ROUND_ROBIN_TIME_INTERVAL);
-                // Simular a execução do programa round-robin
-                while (1) {
-                    // Inserir aqui o código do programa round-robin
-                }
-            } else {
-                // Pegar o próximo programa da fila round-robin que ainda não está em execução
-                ProgramNode *next_program = get_next_round_robin_program();
-                if (next_program != NULL) {
-                    exec_program(&next_program);
-                }
+            // Implementação para programas round-robin
+            while (1) {
+                printf("Round-Robin: Executando programa %s\n", prog.name);
+                sleep(0.5);
             }
         }
     }
 }
 
-
-program* get_next_real_time_program(ProgramNode *real_time_queue) {
-    program *next_program = NULL;
-    ProgramNode *current = real_time_queue;
-    time_t current_time = time(NULL);
-    int min_initial_time = INT_MAX;
-
-    while (current != NULL) {
-        if (current->prog.initial_time < min_initial_time && current->prog.initial_time >= current_time) {
-            // Encontrou um programa com instante inicial menor
-            next_program = &(current->prog);
-            min_initial_time = current->prog.initial_time;
-        } else if (current->prog.initial_time == min_initial_time && current->prog.duration < next_program->duration) {
-            // Encontrou um programa com mesmo instante inicial, mas menor duração
-            next_program = &(current->prog);
-        }
-        current = current->next;
-    }
-
-    return next_program;
-}
-
-program* get_next_priority_program(ProgramNode *priority_queues[]) {
-    program *next_program = NULL;
-    int i = 0;
-
-    // Procurar na fila de prioridade mais alta (prioridade 1) e subsequentemente
-    while (i < 7 && next_program == NULL) {
-        if (priority_queues[i] != NULL) {
-            next_program = &(priority_queues[i]->prog);
-        }
-        i++;
-    }
-
-    return next_program;
-}
-
-program* get_next_round_robin_program(ProgramNode **round_robin_queue) {
-    program *next_program = NULL;
-
-    // Verificar se há algum programa na fila de round-robin
-    if (*round_robin_queue != NULL) {
-        next_program = &((*round_robin_queue)->prog);
-    }
-
-    return next_program;
-}
-
-program* get_next_program(ProgramNode *real_time_queue, ProgramNode *priority_queues[], ProgramNode *round_robin_queue) {
-    program *next_program = NULL;
-
-    // Primeiro, verificar se há programas em tempo real
-    next_program = get_next_real_time_program(real_time_queue);
-
-    // Se não houver programas em tempo real, verificar os de prioridade
-    if (next_program == NULL) {
-        next_program = get_next_priority_program(priority_queues);
-    }
-
-    // Se não houver programas de prioridade, verificar os de round-robin
-    if (next_program == NULL) {
-        next_program = get_next_round_robin_program(&round_robin_queue);
-    }
-
-    return next_program;
-}
-
-void start_program_handler(int signum) {
-    // Obter o próximo programa a ser executado de acordo com a política de escalonamento
-    program *next_program = get_next_program(real_time_queue, priority_queues, round_robin_queue);
-
-    // Verificar se há algum programa disponível para execução
-    if (next_program != NULL) {
-        // Executar o programa
-        execute_program(*next_program);
-    }
-}
-
-void stop_program_handler(int signum) {
-    // Implemente a lógica para interromper a execução do programa aqui
-}
-
-void alarm_handler(int signum) {
-    // Tratamento do sinal de alarme
-    printf("Programa interrompido devido ao sinal de alarme.\n");
-    exit(EXIT_SUCCESS);
-}
-int main(void) {
+void recebe_info(ProgramNode **real_time_queue, ProgramNode *priority_queues[], ProgramNode **round_robin_queue, int pipe_fd) {
+    printf("Recebendo informações...\n\n");
     program program;
-    ProgramNode *real_time_queue = NULL; 
-    ProgramNode *priority_queues[7] = {NULL}; 
-    ProgramNode *round_robin_queue = NULL; 
+    while (read(pipe_fd, &program, sizeof(program)) > 0) {
+        printf("Programa: %s\n", program.name);
+        printf("Tipo: %c\n", program.type);
+        if (program.type == 'P') {
+            printf("Prioridade: %d\n", program.priority);
+            insert_program(&priority_queues[(program.priority - 1)], program, 2); // Prioridade 2 para programas de prioridade
+        } else if (program.type == 'T') {
+            printf("Instante Inicial: %d\n", program.initial_time);
+            printf("Duração: %d\n", program.duration);
+            insert_program(real_time_queue, program, 1); // Prioridade 1 para programas de tempo real
+        } else {
+            insert_program(round_robin_queue, program, 3); // Prioridade 3 para programas round-robin
+        }
+        printf("\n");
+    }
+}
 
+int main(void) {
+    // Configurar o handler para o sinal SIGUSR1
+    signal(SIGUSR1, signal_handler);
+
+    program program;
     int pipe_fd = 0; // Use o descritor de arquivo correto aqui
 
+    // Receber informações e montar as filas de programas
     recebe_info(&real_time_queue, priority_queues, &round_robin_queue, pipe_fd);
 
-    printf("Programas Real Time:\n");
-    print_program_list(real_time_queue);
-    printf("Programas Prioridade:\n");
-    for (int i = 0; i < 7; i++) {
-        printf("Prioridade %d:\n", i+1);
-        print_program_list(priority_queues[i]);
-    }
-    printf("Programas Round Robin:\n");
-    print_program_list(round_robin_queue);
-    // while(1){
-    //     // Executar programas de cada tipo
-    //     if (real_time_queue != NULL) {
-    //         exec_program(&real_time_queue);
-    //     }else{
-    //         for (int i = 0; i < 8; i++) {
-    //             if(i == 0){
-    //                 if (priority_queues[0] != NULL){
-    //                     exec_program(&priority_queues[0]);
-    //                 }
-    //             }else{
-    //                 if (priority_queues[i] != NULL && priority_queues[i-1]==NULL) {
-    //                     exec_program(&priority_queues[i]);
-    //                 }
-    //             }
-    //         }
-    //         if (round_robin_queue != NULL) {
-    //             exec_program(&round_robin_queue);
-    //         }
-    //     }
-    // }
+    // Loop principal para execução dos programas
+    while (1) {
+        // Obter o próximo programa a ser executado
+        ProgramNode *next_program = NULL;
+        if (real_time_queue != NULL) {
+            next_program = real_time_queue;
+        } else {
+            for (int i = 0; i < 7; i++) {
+                if (priority_queues[i] != NULL) {
+                    next_program = priority_queues[i];
+                    break;
+                }
+            }
+            if (next_program == NULL && round_robin_queue != NULL) {
+                next_program = round_robin_queue;
+            }
+        }
 
-    // Liberar memória das listas de programas
-    free_program_list(real_time_queue);
-    for (int i = 0; i < 8; i++) {
-        free_program_list(priority_queues[i]);
+        // Executar o próximo programa se ele existir
+        if (next_program != NULL) {
+            exec_program(&next_program);
+            current_program_node = next_program;
+        } else {
+            // Se não houver mais programas para executar, aguardar um sinal
+            sleep(1);
+        }
     }
-    free_program_list(round_robin_queue);
 
     return 0;
 }
